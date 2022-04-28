e4924c5d5b7526480787f332e72997bbdbf571a2
tsangyi <157486478@qq.com>
2022-04-28 23:44:01 +0800

    。。

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <!-- 第一次使用ant属性 -->
+    <a-button type="primary">primary</a-button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// 引入ant的一个组件
+import { Button } from 'ant-design-vue'
 
-createApp(App).mount('#app')
+createApp(App).use(Button).mount('#app')
