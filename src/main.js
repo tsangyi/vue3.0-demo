236c81890b80b4def23379f9792c98cbfcca7506
tsangyi <157486478@qq.com>
2022-04-28 23:58:54 +0800

    。。

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
+// 引入App.vue这个入口文件
 import App from './App.vue'
+// 引入路由
+import router from './router'
 // 引入ant的一个组件
 import { Button } from 'ant-design-vue'
 
-createApp(App).use(Button).mount('#app')
+createApp(App)
+    .use(Button)
+    .use(router)
+    .mount('#app')
