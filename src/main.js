db641ed67eaa12e46cc249697922ef1464b5701f
tsangyi <157486478@qq.com>
2022-04-29 00:39:35 +0800

    。。

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import { createApp } from 'vue'
 // 引入App.vue这个入口文件
 import App from './App.vue'
 // 引入路由
-import router from './router'
+// import router from './router'
 // 引入ant的一个组件
 import { Button } from 'ant-design-vue'
 
 createApp(App)
     .use(Button)
-    .use(router)
+    // .use(router)
     .mount('#app')
