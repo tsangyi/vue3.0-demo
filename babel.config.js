8478ac17f1a13bbf9e74f3ea2372bfd06e3ba699
tsangyi <157486478@qq.com>
2022-04-28 23:44:35 +0800

    。。

--- babel.config.js
+++ babel.config.js
@@ -3,3 +3,16 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ]
 }
+
+// yarn add babel-plugin-import --dev 安装好后，按需加载
+module.exports = {
+  presets: ["@vue/app"],
+  plugins: [
+    [
+      "import",
+      // style: true 会加载 less 文件
+      { libraryName: "ant-design-vue", libraryDirectory: "es", style: true }
+    ]
+  ]
+};
+
