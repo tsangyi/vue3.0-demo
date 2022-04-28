2fdb2ea9aa1de53d4be4f2dd32c059b46d59c78c
tsangyi <157486478@qq.com>
2022-04-29 00:57:23 +0800

    ..

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hello">
+          <a-button type="primary" size="small" >primary</a-button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
